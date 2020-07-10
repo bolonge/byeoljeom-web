027fe6e875069167095384db41ffb8742b2eba93
bolonge <xxs456@naver.com>
2020-07-10 21:58:44 +0900

    검색인풋생성

---
+++ src/Components/Input/SearchInput/SearchInput.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "../../../typed-components";
+
+const Input = styled.input``;
+
+const SearchInput: React.FunctionComponent = () => {
+  return <Input></Input>;
+};
+
+export default SearchInput;
