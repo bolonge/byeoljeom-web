cafc70089dfbdb55c6aa0678e81c08ffef1ca808
bolonge <xxs456@naver.com>
2020-07-13 00:07:05 +0900

    아바타 추가

---
+++ src/Components/Avatar/Avatar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "../../typed-components";
+
+const getSize = (size: string) => {
+  let number;
+  if (size === "sm") {
+    number = 30;
+  } else if (size === "md") {
+    number = 50;
+  } else if (size === "lg") {
+    number = 150;
+  }
+  return `
+          width:${number}px;
+          height:${number}px;
+          `;
+};
+
+const Container = styled.div<{ size: string; url: string }>`
+  ${(props) => getSize(props.size)}
+  background-image:url(${(props) => props.url});
+`;
+
+interface IProp {
+  size: string;
+  className?: string;
+  url: string;
+}
+
+const Avatar: React.FunctionComponent<IProp> = ({
+  size = "sm",
+  className,
+  url,
+}) => {
+  return <Container size={size} className={className} url={url} />;
+};
+
+export default Avatar;
