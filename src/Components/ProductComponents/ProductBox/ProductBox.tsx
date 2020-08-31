4e8e2bb8480568af2de7602441709d3c300ad514
bolonge <xxs456@naver.com>
2020-09-01 00:10:41 +0900

    박스 그림자 추가

--- src/Components/ProductComponents/ProductBox/ProductBox.tsx
+++ src/Components/ProductComponents/ProductBox/ProductBox.tsx
@@ -8,7 +8,12 @@ const Wrapper = styled.div`
   width: auto;
   height: 300px;
   ${(props) => props.theme.whiteBox};
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.2s cubic-bezier(0.25, 0.8, 0.25, 1);
   cursor: pointer;
+  &:hover {
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  }
   ${media.tablet} {
     width: auto;
   }
@@ -25,6 +30,7 @@ const PhotoContainer = styled.div<{ url: string }>`
   border: 1px solid blue;
   align-items: center;
   justify-content: center;
+
   background-image: url(${(props) => props.url});
   background-size: cover;
   border-radius: 10px;
