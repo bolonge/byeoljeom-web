b6c94e38144a665ac07d8134521c683616ef0d85
bolonge <xxs456@naver.com>
2020-09-14 21:33:07 +0900

    상품페이지 추가

---
+++ src/Routes/Product/ProductContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ProductPresenter from "./ProductPresenter";
+
+const ProductContainer: React.FunctionComponent = () => {
+  return <ProductPresenter></ProductPresenter>;
+};
+
+export default ProductContainer;
---
+++ src/Routes/Product/ProductPresenter.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "../../typed-components";
+
+const Wrapper = styled.div``;
+
+const ProductPresenter: React.FunctionComponent = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default ProductPresenter;
