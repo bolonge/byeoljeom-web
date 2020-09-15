45446f6b59d1c7c205104640e44677ca1472f9ce
bolonge <xxs456@naver.com>
2020-09-15 22:35:43 +0900

    프로필 페이지

---
+++ src/Routes/Profile/Profile.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "../../typed-components";
+
+const MainPage = styled.div``;
+
+const Profile: React.FunctionComponent = () => {
+  return <MainPage></MainPage>;
+};
+
+export default Profile;
